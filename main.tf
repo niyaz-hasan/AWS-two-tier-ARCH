e111addc70cdb13428bbb400a8d80f68f6cd3666
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-02 19:30:16 +0530

    terraform apply

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ module "rds" {
   source               = "./modules/rds"
   name                 = var.name
   private_subnets      = module.vpc.private_subnets
-  db_username          = "admin"
-  db_password          = "y7Yfyr8oHIUvkDs4"
+  db_username          = var.db_username
+  db_password          = var.password
   rds_security_group_ids  = [module.sg_group.rds_security_group_id]
 }
--- variables.tf
+++ variables.tf
@@ -15,3 +15,17 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "db_username" {
+  description = "Name of the region"
+  type        = string
+  default     = "admin"
+}
+
+
+
+variable "db_password" {
+  description = "Name of the region"
+  type        = string
+  default     = "y7Yfyr8oHIUvkDs4"
+}
